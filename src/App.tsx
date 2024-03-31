152b8c6b0052e0ef4345f33ae320c22cbafb8354
dadondev <dadondev03@gmail.com>
2024-03-31 23:36:00 +0500

    helloo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Chats from "./pages/Chats";
+import Login from "./pages/Login";
+import ChatRoom from "./pages/ChatRoom";
+import "./index.css";
+import { CssBaseline } from "@mui/material";
 
-function App() {
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Chats />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/chat-room/:id",
+    element: <ChatRoom />,
+  },
+]);
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <RouterProvider router={router} />
+    </>
   );
-}
+};
 
 export default App;
